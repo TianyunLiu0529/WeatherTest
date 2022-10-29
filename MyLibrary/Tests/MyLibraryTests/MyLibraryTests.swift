b8e1b49a1bc8c5e6b70d225e9a0b140b87d0f932
TianyunLiu <liut2@oregonstate.edu>
2022-10-29 09:16:06 +0000

    add tests

--- MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
+++ MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-import MyLibrary
+@testable import MyLibrary
 
 final class MyLibraryTests: XCTestCase {
     func testIsLuckyBecauseWeAlreadyHaveLuckyNumber() async {
@@ -67,6 +67,68 @@ final class MyLibraryTests: XCTestCase {
 
         // Then
         XCTAssertNil(isLuckyNumber)
+    }
+    func testWeather() async throws{
+        //Given
+        let jsonString: String = """
+            {
+                "coord": {
+                    "lon": 10.99,
+                    "lat": 44.34
+                },
+                "weather": [
+                    {
+                        "id": 501,
+                        "main": "Rain",
+                        "description": "moderate rain",
+                        "icon": "10d"
+                    }
+                ],
+                "base": "stations",
+                "main": {
+                    "temp": 298.48
+                    "feels_like": 298.74,
+                    "temp_min": 297.56,
+                    "temp_max": 300.05,
+                    "pressure": 1015,
+                    "humidity": 64,
+                    "sea_level": 1015,
+                    "grnd_level": 933
+                },
+                "visibility": 10000,
+                "wind": {
+                    "speed": 0,62,
+                    "deg": 349,
+                    "gust": 1.18
+                },
+                "rain": {
+                    "1h": 3.16
+                },
+                "clouds": {
+                    "all": 100
+                },
+                "dt": 1661870592,
+                "sys": {
+                    "type": 2,
+                    "id": 2075663,
+                    "country": "IT",
+                    "sunrise": 1661834187,
+                    "sunseet": 1661882248
+                },
+                "timezone": 7200,
+                "id": 3163858,
+                "name": "Zocca",
+                "cod": 200
+            }
+        """
+        //When
+
+        let jsonData: Data = jsonString.data(using: .utf8)!
+        let decoder: JSONDecoder = JSONDecoder()
+        let testData: Weather = try! decoder.decode(Weather.self, from: jsonData)
+
+        //Then
+        XCTAssert(testData.main.temp == 298.48)
     }
 
 }
